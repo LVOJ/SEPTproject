b9e8ab35b06ecdbd8d5dada6be4397f09cc42db3
tommychua <s3503207@student.rmit.edu.au>
2017-04-04 13:55:04 +1000

    Pair Programming , Booking implemented. Test cases not written

--- ABS/src/CustomerPanel.java
+++ ABS/src/CustomerPanel.java
@@ -10,6 +10,58 @@ public class CustomerPanel {
     
     public static void BookAppointment(String[] data2)
     {
+    	Booking book = new Booking();
+        ArrayList<String> data=new ArrayList<>();
+        try
+        {
+            FileReader fr=new FileReader("employeeinfo.txt");
+            BufferedReader br=new BufferedReader(fr);
+            String line="";
+            System.out.printf("%1s%10s%20s%12s\n","Employee Name","Day","Time Available","Status");
+            //System.out.println("Employee Name\tDay\tTime Available\tStatus");
+            int i=1;
+            while((line=br.readLine())!=null)
+            {
+                data.add(line);     //save list of bookings
+                String arr[]=line.split(",");
+                System.out.printf("%1s%1s%20s%15s%20s\n",i+"-",arr[0],arr[1],arr[2],arr[3]);
+                //System.out.println(i+"-"+arr[0]+"\t\t"+arr[1]+"\t"+arr[2]+"\t"+arr[3]);
+                i++;
+            }
+            br.close();
+            System.out.println("\nSelect Time i.e (1,2)");
+            String select=in.next();
+            String[] selected = select.split(",");
+            int choice = 0;
+            for(int a=0;a<selected.length;a++){
+                choice = Integer.parseInt(selected[a]);
+            
+                if(choice<=data.size())
+                {
+                    String arr[]=data.get(choice-1).split(",");
+                    if(arr[3].equals("available"))
+                    {
+                        book.BookSlot(data,choice);
+                        BufferedWriter writer2 = new BufferedWriter(new FileWriter("BookingSummaries.txt",true));
+                        String s=data.get(choice-1);
+                        s=s.replace("available", "");
+                        s=s.replace(",", " ");
+                        writer2.write("Customer "+data2[0]+" "+data2[1]+" booked Appointment on "+s);
+                        writer2.newLine();
+                        System.out.println("Customer "+data2[0]+" "+data2[1]+" booked Appointment on "+s);
+                        writer2.close();
+                    }
+                    else
+                        System.out.println("Slot Already Booked");
+                }
+                else
+                    System.out.println("Not Valid Slot Try Again!");
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
        
     }
     public static void ShowBookingTimeTable()
